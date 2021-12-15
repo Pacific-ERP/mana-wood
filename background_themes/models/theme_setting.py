dc9df11e7bc14f6364b1b56ae0808666d749da66
Tepava <mehdi@pacific-erp.com>
2021-12-15 22:09:08 +0000

    faux model_id

--- background_themes/models/theme_setting.py
+++ background_themes/models/theme_setting.py
@@ -3,8 +3,8 @@
 from odoo import models, fields, api
 
 
-class theme_settings(models.Model):
-     _name = 'themes.settings'
+class Settings(models.Model):
+     _name = 'theme.setting'
      _description = 'Configuration du themes'
      nav_font_color = fields.Char()
      nav_background_color = fields.Char()
