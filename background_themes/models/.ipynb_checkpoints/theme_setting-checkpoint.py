4438826fbe744c2d96a23b669bd10054883f973a
Tepava <mehdi@pacific-erp.com>
2021-12-16 21:29:28 +0000

    module créer + base configuration

--- background_themes/models/.ipynb_checkpoints/theme_setting-checkpoint.py
+++ background_themes/models/.ipynb_checkpoints/theme_setting-checkpoint.py
@@ -6,8 +6,8 @@ from odoo import models, fields, api
 class Settings(models.Model):
      _name = 'theme.setting'
      _description = 'Configuration du themes'
-     nav_font_color = fields.Char()
-     nav_background_color = fields.Char()
-     background_color = fields.Char()
-#    society = fields.many2one('patient.name', 'name')
+     nav_font_color = fields.Char(string="Couleur navigation texte")
+     nav_background_color = fields.Char(string="Couleur barre de navigation")
+     background_color = fields.Char(string="Couleur du menu")
+#     company = fields.Many2one(comodel_name='res.company',related='id')
 
--- background_themes/models/__init__.py
+++ background_themes/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import theme_setting
+from . import test_settings
